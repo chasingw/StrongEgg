b3f8a959e3891ddad72a9058c617e9ef663e6bc8
chasingw <wangd101@163.com>
2016-05-20 21:21:31 +0800

    init project package

--- app/src/main/java/com/chasingw/strongegg/acitvity/MainActivity.java
+++ app/src/main/java/com/chasingw/strongegg/acitvity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.chasingw.strongegg;
+package com.chasingw.strongegg.acitvity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.chasingw.strongegg.R;
 
 public class MainActivity extends AppCompatActivity {
 
